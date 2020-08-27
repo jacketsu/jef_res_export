4703c32788d9ba77ddbe9c4bfd45e5bababd8d96
WEISU <jacketsu@hotmail.com>
2020-08-27 17:01:24 +0800

    init

--- export.py
+++ export.py
@@ -5,7 +5,7 @@ from requests import get, post, delete
 
 
 def accession_list_v2():
-    directory = "/media/tx-deepocean/Data/TMP"
+    directory = "/media/tx-deepocean/Data/DICOMS/CT_Lung"
     folders = []
     for pid in os.listdir(directory):
         if "." in pid:
@@ -15,21 +15,23 @@ def accession_list_v2():
         path = "/".join([path, pid_f[0]])
         for subf in os.listdir(path):
             fname = "/".join([pid, subf])
-            count = len(os.listdir("/".join([path, subf])))
+            # count = len(os.listdir("/".join([path, subf])))
             # mtime = time.ctime(os.path.getmtime("/".join([directory, pid])))
             mtime = os.path.getmtime("/".join([directory, pid]))
-            folders.append([fname, mtime, count, pid])
-        break
-    folders = sorted(folders, key=lambda f:-os.path.getmtime("/".join([directory, f[3]])))
+            folders.append([fname, mtime pid])
+        
+    # folders = sorted(folders, key=lambda f:-os.path.getmtime("/".join([directory, f[3]])))
     return folders
 
 
 
 if __name__ == "__main__":
     folders = accession_list_v2()
-    sid = str(folders[0][0].split('/')[-1])
-    predict_url = "/".join(['http://127.0.0.1:3000/api2/series', sid, 'predict', 'ct_lung'])
-    print(predict_url)
-    r = get(predict_url)
-    j = r.json()
-    print(j)
+    for f in folders:
+        sid = str(f[0].split('/')[-1])
+        predict_url = "/".join(['http://127.0.0.1:3000/api2/series', sid, 'predict', 'ct_lung'])
+        print(predict_url)
+        r = get(predict_url)
+        if not r:
+            j = r.json()
+            print(j)
